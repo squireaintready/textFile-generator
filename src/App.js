f34a1568a94e7175f89f865d4d7f0015219c45ca
Samuel Jo <jo.sunghak@gmail.com>
2022-03-28 13:42:45 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
+import TextField from "@mui/material/TextField";
 
 function App() {
+  const [text, setText] = useState("");
+  const [title, setTitle] = useState('')
+
+  const handleClick = () => {
+    let link = document.getElementById("downloadFile");
+    link.href = generateTextFile(text);
+    link.download = title;
+    link.style.display = "inline-block";
+  };
+
+  const generateTextFile = (text) => {
+    let textFile = null;
+    let data = new Blob([text], { text: "text/plain" });
+    if (textFile !== null) {
+      window.URL.revokeObjectURL(textFile);
+    }
+    textFile = window.URL.createObjectURL(data);
+    return textFile;
+  };
+
+  const hideDownload = () => {
+    let link = document.getElementById("downloadFile");
+    setTimeout(() => {
+      link.style.display = "none";
+    }, 1500)
+  }
+
+  const handleTextChange = (e) => {
+    setText(e.target.value);
+  };
+
+  const handleTitleChange = (e) => {
+    setTitle(e.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div className="app">
+      <TextField
+        id="title"
+        label="Title"
+        value={title}
+        onChange={handleTitleChange}
+        variant="standard"
+        margin="dense"
+      />
+      <TextField
+        id="dummyText"
+        fullWidth
+        multiline
+        label="Enter New Text"
+        value={text}
+        onChange={handleTextChange}
+        variant="outlined"
+        margin='normal'
+      />
+      <div className='actions'>
+        <button id="btnCreateFile" onClick={handleClick}>
+          Generate File
+        </button>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          download="info.txt"
+          href="alt"
+          id="downloadFile"
+          style={{ display: "none" }}
+          onClick={hideDownload}
         >
-          Learn React
+          Download
         </a>
-      </header>
+      </div>
     </div>
   );
 }
